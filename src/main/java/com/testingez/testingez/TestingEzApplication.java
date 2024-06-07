57a0a769adfcae1828208a22d561cc94e915c89b
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-06-07 13:18:43 +0300

    separate AuthenticationController in two different - LoginController and RegisterController

--- src/main/java/com/testingez/testingez/TestingEzApplication.java
+++ src/main/java/com/testingez/testingez/TestingEzApplication.java
@@ -10,6 +10,12 @@ public class TestingEzApplication {
 		SpringApplication.run(TestingEzApplication.class, args);
 	}
 
-	// TODO: work on sign-up.html
+	// TODO: 1.Remove the confirmPasswords logic in the controller
+	//		 2.Separate the AuthenticationController into two different
+	// 		 3.Add password encoding
+	//   	 4.Create new template for /user/home endpoint
+	// 	     5.Add pseudo-session, a.k.a. CurrentUser
+	// 		 6.Remove the default browser behavior from the register and login forms
+	//		 7.Clear the templates by adding more fragments
 
 }
