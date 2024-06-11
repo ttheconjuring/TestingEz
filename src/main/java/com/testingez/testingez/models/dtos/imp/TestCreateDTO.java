4ef64efa2e6d919ff48a64fee982fc079004981d
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-06-11 11:08:12 +0300

    validations added to TestCreateDTO, code generator button added in html

--- src/main/java/com/testingez/testingez/models/dtos/imp/TestCreateDTO.java
+++ src/main/java/com/testingez/testingez/models/dtos/imp/TestCreateDTO.java
@@ -13,28 +13,30 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TestCreateDTO {
 
-    @NotBlank
+    @NotNull(message = "Test name should not be null.")
+    @Size(min = 5, max = 25, message = "Test name should be between 5 and 25 symbols.")
     private String name;
 
-    @NotNull
+    @NotNull(message = "Description should not be null.")
+    @Size(max = 50, message = "Description should be up to 50 symbols.")
     private String description;
 
-    @NotBlank
+    @NotNull(message = "Code should not be null.")
+    @Pattern(regexp = "^(?=.*[A-Za-z].*[A-Za-z])(?=.*\\d.*\\d)(?=.*[!@#$%^&*].*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{6}$")
     private String code;
 
-    @NotNull
-    @Digits(integer = 1, fraction = 0)
-    private String responseTime;
+    @NotNull(message = "Response time should not be null.")
+    @Positive(message = "Response time should be a positive number.")
+    private Integer responseTime;
 
-    @Positive
-    @NotNull
-    private String passingScore;
+    @NotNull(message = "Passing score should not be null.")
+    private Integer passingScore;
 
-    @Positive
-    @NotNull
-    private String questionsCount;
+    @NotNull(message = "Questions count should not be null.")
+    @Positive(message = "Questions count should be positive number.")
+    private Integer questionsCount;
 
-    @NotBlank
+    @NotBlank(message = "Test status should not be null or empty.")
     private TestStatus status;
 
 }
