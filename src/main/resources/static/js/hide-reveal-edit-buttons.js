fe8929c8bd4ed90a7ba7dcc2dc6982bab174b457
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-06-26 12:43:13 +0300

    the edit profile button is making the input fields enabled and allows you to edit the data

--- src/main/resources/static/js/hide-reveal-edit-buttons.js
+++ src/main/resources/static/js/hide-reveal-edit-buttons.js
@@ -1,7 +1,15 @@
-const editButton = document.getElementById('edit-button');
-const editButtonsDiv = document.getElementById('edit-buttons-div');
+const editProfileButton = document.getElementById('edit-profile-button');
+const editProfileButtonsDiv = document.getElementById('edit-profile-buttons-div');
+const inputFields = document.querySelectorAll('input');
 
-editButton.addEventListener('click', function () {
-    editButtonsDiv.style.display = 'flex';
+editProfileButton.addEventListener('click', function () {
+    editProfileButtonsDiv.style.display = 'flex';
     this.style.display = 'none';
+    inputFields.forEach(function (input) {
+        input.disabled = false;
+        input.readOnly = false;
+
+        const fieldValue = input.value;
+        input.value = fieldValue.split(': ')[1];
+    })
 })
