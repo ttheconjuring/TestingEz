8a292175270ce1ad6ca86ebd37f591e270f99f41
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-07-02 14:50:13 +0300

    All tests and All users tabs added for administators only

--- src/main/java/com/testingez/testingez/security/UserDetailsServiceImpl.java
+++ src/main/java/com/testingez/testingez/security/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.testingez.testingez.security;
 
 import com.testingez.testingez.models.entities.User;
 import com.testingez.testingez.repositories.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,14 +21,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return this.userRepository.findByUsername(username)
                 .map(UserDetailsServiceImpl::map)
-                .orElseThrow(() -> new UsernameNotFoundException("No user with username: \"" + username + "\" found!"));
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with username: \"" + username + "\""));
     }
 
     private static UserDetails map(User user) {
         return org.springframework.security.core.userdetails.User
                 .withUsername(user.getUsername())
                 .password(user.getPassword())
-                .authorities(List.of())
+                .authorities(List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().getRole().name())))
                 .disabled(false)
                 .build();
     }
