b28a859f1d35d3c0643c3ec5ebfcb1dae486cffb
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-07-05 14:53:36 +0300

    ninja api microservice is now separated application

--- src/main/java/com/testingez/testingez/api/ninja/repositories/TriviaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.testingez.testingez.api.ninja.repositories;
-
-import com.testingez.testingez.api.ninja.entities.Trivia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TriviaRepository extends JpaRepository<Trivia, Long> {
-}
