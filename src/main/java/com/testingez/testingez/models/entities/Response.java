71ba9668765060429729952f6cd36f3cd56f52ca
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-07-05 15:45:41 +0300

    Response Entity created

---
+++ src/main/java/com/testingez/testingez/models/entities/Response.java
@@ -0,0 +1,36 @@
+package com.testingez.testingez.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "responses")
+public class Response {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "response_text", nullable = false)
+    private String responseText;
+
+    @Column(name = "is_correct", nullable = false)
+    private Boolean isCorrect;
+
+    @Column(name = "submitten_on", nullable = false)
+    private LocalDateTime submittedOn;
+
+    @OneToOne
+    private User user;
+
+    @OneToOne
+    private Question question;
+
+}
