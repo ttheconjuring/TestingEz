c4b9846c69b5ae0d6338afd51ab94235bd1ee5d8
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-07-05 15:59:57 +0300

    api.ninjas removed

--- src/main/java/com/testingez/testingez/api/ninja/repositories/FactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.testingez.testingez.api.ninja.repositories;
-
-import com.testingez.testingez.api.ninja.entities.Fact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FactRepository extends JpaRepository<Fact, Long> {
-}
