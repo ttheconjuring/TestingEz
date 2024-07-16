0262bc1f872e34d7cc370f18bfff93b6cce4714f
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-07-16 18:36:21 +0300

    working on my tests page, pagination is made, but no sorting and template

--- src/main/java/com/testingez/testingez/web/UserController.java
+++ src/main/java/com/testingez/testingez/web/UserController.java
@@ -1,6 +1,9 @@
 package com.testingez.testingez.web;
 
+import com.testingez.testingez.models.dtos.exp.TestPeekDTO;
 import com.testingez.testingez.models.dtos.exp.UserProfileDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -14,5 +17,7 @@ public interface UserController {
     String edit(Model model);
 
     String edit(UserProfileDTO userProfileData, BindingResult bindingResult, RedirectAttributes redirectAttributes);
+
+    String userTests(Pageable pageable, Model model);
 
 }
