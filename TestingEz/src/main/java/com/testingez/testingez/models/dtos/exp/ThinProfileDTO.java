33ecc5330ce0639d079fa576187066ae0981250f
ttheconjuring <90863683+ttheconjuring@users.noreply.github.com>
2024-09-14 18:44:02 +0300

    ThinProfileDTO created for displaying only the avatar and the username of the profile

---
+++ TestingEz/src/main/java/com/testingez/testingez/models/dtos/exp/ThinProfileDTO.java
@@ -0,0 +1,16 @@
+package com.testingez.testingez.models.dtos.exp;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ThinProfileDTO {
+
+    private String username;
+
+    private String avatarUrl;
+
+}
